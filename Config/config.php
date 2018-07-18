2c9b17daca65536ca98ea02e30134e1981c190dd
scottshipman <k.scott.shipman@gmail.com>
2018-07-18 14:28:18 -0400

    Stub out new bundle

---
+++ Config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Digital Media Solutions, LLC
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+return [
+    'name'        => 'Api Services',
+    'description' => 'Proxy function for service calls as an API.',
+    'version'     => '1.0',
+    'author'      => 'Mautic',
+
+    'services' => [
+        'models'       => [
+        ],
+    ],
+];
