f25e515d33d75bdced359daa6c056f1bc7bd47b5
scottshipman <k.scott.shipman@gmail.com>
2018-07-24 12:56:19 -0400

    add css to make it prettier

--- Views/Integration/form.html.php
+++ Views/Integration/form.html.php
@@ -2,6 +2,6 @@
     Click the "Features" tab to configure Api Services.
 </div>
 <?php
-echo $view['assets']->includeScript('plugins/MauticApiServicesBundle/Assets/build/apiservices.min.js', 'apiServices_loadServiceForm', 'apiServices_loadServiceForm');
-// echo $view['assets']->includeStylesheet('plugins/MauticContactClientBundle/Assets/build/contactclient.min.css');
+    echo $view['assets']->includeScript('plugins/MauticApiServicesBundle/Assets/build/apiservices.min.js', 'apiServices_loadServiceForm', 'apiServices_loadServiceForm');
+    echo $view['assets']->includeStylesheet('plugins/MauticApiServicesBundle/Assets/build/apiservices.min.css');
 ?>
